b996e050d0ca06931bb5e9a497259f01be28ccb3
nazayca <nazayca@gmail.com>
2024-01-06 00:08:15 -0600

    new addes

--- cypress/e2e/02-Navigations.cy.js
+++ cypress/e2e/02-Navigations.cy.js
@@ -1,14 +1,22 @@
 /// <reference types="cypress"/>
-describe('Cypress Navigation', () => {
-    it('Go to ULR, refresh, navigate forward and back', () => {
-        cy.visit('https://techglobal-training.com/')
-        //refresh page
-        cy.reload()
-       // nevigate to another page
-        cy.visit('https://www.google.com/')
-      //nevigate back
-      cy.go('back')
 
-      cy.go('forward')
-    })
+describe('Cypress Navigation', () => {
+
+  it('Go to ULR, refresh, navigate forward and back', () => {
+      cy.visit('https://techglobal-training.com/')
+
+      // refresh the page
+      cy.reload()
+
+      // navigate to another page
+      cy.visit('https://www.google.com/')
+
+      // navigate back to TechGlobal website
+      //cy.go('back')
+      cy.go(-1)
+
+      // navigate forward to Google website
+      //cy.go('forward')
+      cy.go(1)
+  })
 })
